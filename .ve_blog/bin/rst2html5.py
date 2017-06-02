7d3fcc15a283011d6dc1cc473fb8cefc2e1a45eb
DInnaD <innadanylevska@gmail.com>
2017-06-02 16:47:41 +0300

    Add virtualenv and setup pelican.

---
+++ .ve_blog/bin/rst2html5.py
@@ -0,0 +1,4 @@
+#!/opt/lampp/htdocs/Blog/gitproject/DInnaD.github.com/.ve_blog/bin/python2
+# EASY-INSTALL-SCRIPT: 'docutils==0.14rc1','rst2html5.py'
+__requires__ = 'docutils==0.14rc1'
+__import__('pkg_resources').run_script('docutils==0.14rc1', 'rst2html5.py')
